e996788a5ee9872f823e7ca00f93c4305882b412
Aidan T. Manning <periodicaidan@gmail.com>
2020-10-21 21:19:51 -0500

    wrote the rest of the server logic

--- server.js
+++ server.js
@@ -1,29 +1,54 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const PORT = process.env.PORT || 3900;
+const DB_PATH = 'db/db.json';
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
 
 // Allows Express to parse data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    // TODO: Return notes.html
+    return res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    // TODO: Return all saved notes
+    fs.readFile(DB_PATH, 'utf-8', (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            return res.send('Server error, sorry for the inconvenience!');
+        }
+
+        return res.json(JSON.parse(data));
+    });
 });
 
-app.post('/api/notes', (req, res) => {
-    // TODO: Save a new note
+app.post('/api/notes', async (req, res) => {
+    const db = JSON.parse(await readFileAsync(DB_PATH, 'utf-8'));
+    db.push(req.body);
+    writeFileAsync(DB_PATH, JSON.stringify(db));
+    return res.json(db);
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    // TODO: Delete the note with ID `id`
+app.delete('/api/notes/:id', async (req, res) => {
+    const db = JSON.parse(await readFileAsync(DB_PATH, 'utf-8'));
+    db.splice(req.params.id);
+    writeFileAsync(DB_PATH, JSON.stringify(db));
+    return res.json(db);
 });
 
 // Fallback route thing
 // NB: This should always be the last route
 app.get('*', (req, res) => {
-    // TODO: Return index.html
+    return res.sendFile('public/index.html');
 });
+
+app.listen(PORT);
